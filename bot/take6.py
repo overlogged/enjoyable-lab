2dbb8ad49201e213b111dda05b880c53f28083f0
ljw <nicekingwei@sina.com>
2017-08-01 23:32:07 +0800

    bot

---
+++ bot/take6.py
@@ -0,0 +1,26 @@
+import re
+import time
+import os
+
+def onQQMessage(bot, contact, member, content):
+    time.sleep(2)
+        return (contact.qq=="1761440454" or contact.mark=="admin")
+    m = re.match(r"\/log (\d+)",content)
+    if m and ((contact.qq=="659529585") or admin(contact)):
+        try:
+            n = int(m.groups()[0])
+            fin = open("~/take6/log.txt","rt")
+            lines = fin.readlines()
+            skip=len(lines)-n
+            out=""
+            for line in lines:
+                if skip>0:
+                    skip -= 1
+                else:
+                    out+=line
+            bot.SendTo(contact,out.rstrip('\n'))
+            fin.close()
+        except:
+            pass
+    elif content=="reset" and (admin(member) or admin(contact)):
+        os.system("pkill java & cd ~/take6/server & sbt run &")
